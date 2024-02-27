f523626bc6381e0ec05599f9633625e6066555b4
Victor Jerrysson <victorjerrysson@gmail.com>
2024-02-27 10:00:04 -0300

    fix: Fixing the array desestructuring error

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,16 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { api } from "@/data/api";
+
+import { Product } from "@/data/types/product";
+
+async function getFeaturedProducts(): Promise<Product[]> {
+
+  const response = await api('/products/featured')
+  
+  const products: Product[] = await response.json().then((data) => data.data)
+
+  return products
+}
+
 export default async function Home() {
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts()
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
 
       {/* main link */}
       <Link 
-        href="/" 
+        href={`/products/${highlightedProduct?.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
           <Image
-            src="/moletom-never-stop-learning.png"
+            src={highlightedProduct?.image}
             className="group-hover:scale-105 transition-transform duration-500"
             width={920}
             height={920}
@@ -19,8 +34,11 @@ export default async function Home() {
           />
 
           <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">Moletom Never Stop Learning</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">R$129</span>
+            <span className="text-sm truncate">{highlightedProduct?.title}</span>
+            <span 
+              className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {highlightedProduct?.price}
+            </span>
           </div>
       </Link>
 
---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+
+import data from '../data.json'
+
+export async function GET() {
+    const featuredProducts = data.products.filter((product) => product.featured)
+
+    return NextResponse.json({
+        data: featuredProducts
+    })
+
+}
